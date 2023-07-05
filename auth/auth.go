54384120be28f52b8a76ea3ea53b944960d1862f
logotipiwe <german.reus@bk.ru>
2023-07-05 14:06:32 +0600

    Init

--- auth/auth.go
+++ auth/auth.go
@@ -2,19 +2,33 @@ package auth
 
 import (
 	"encoding/json"
-	"errors"
-	env "github.com/logotipiwe/dc_go_env_lib"
 	"net/http"
-	"net/url"
 )
 
-type User struct {
-	Sub        string `json:"sub"`
-	Name       string `json:"name"`
-	GivenName  string `json:"given_name"`
-	FamilyName string `json:"family_name"`
-	Picture    string `json:"picture"`
-	Locale     string `json:"locale"`
+type DcUser struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+}
+
+func FetchUserData(r *http.Request) (DcUser, error) {
+	getUrl := "http://idp/getUser"
+	println(getUrl)
+	request, _ := http.NewRequest("GET", getUrl, nil)
+	authCookie, err := r.Cookie("access_token")
+	if err == nil {
+		request.AddCookie(authCookie)
+	}
+
+	client := &http.Client{}
+	res, _ := client.Do(request)
+	defer res.Body.Close()
+	var answer DcUser
+	err = json.NewDecoder(res.Body).Decode(&answer)
+	if err != nil {
+		return DcUser{}, err
+	}
+	return answer, nil
 }
 
 func GetAccessTokenFromCookie(r *http.Request) (string, error) {
@@ -29,42 +43,4 @@ func GetAccessTokenFromCookie(r *http.Request) (string, error) {
 		accessToken = ""
 	}
 	return accessToken, nil
-}
-
-func GetUserData(r *http.Request) (User, error) {
-	accessToken, err := GetAccessTokenFromCookie(r)
-	if err != nil {
-		return User{}, err
-	}
-	return GetUserDataFromToken(accessToken)
-}
-
-func GetUserDataFromToken(accessToken string) (User, error) {
-	bearer := "Bearer " + accessToken
-	getUrl := "https://www.googleapis.com/oauth2/v3/userinfo"
-	request, _ := http.NewRequest("GET", getUrl, nil)
-	request.Header.Add("Authorization", bearer)
-
-	client := &http.Client{}
-	res, _ := client.Do(request)
-	defer res.Body.Close()
-	var answer User
-	err := json.NewDecoder(res.Body).Decode(&answer)
-	if err != nil {
-		return User{}, err
-	}
-	if answer.Sub != "" {
-		return answer, nil
-	} else {
-		return answer, errors.New("WTF HUH")
-	}
-}
-
-func GetLoginUrl() string {
-	loginUrl, _ := url.Parse(env.GetCurrUrl() + "/oauth2/auth")
-	q := loginUrl.Query()
-	q.Set("redirect", env.GetCurrUrl()+env.GetSubpath())
-	loginUrl.RawQuery = q.Encode()
-
-	return loginUrl.String()
 }
